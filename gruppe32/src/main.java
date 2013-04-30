4e5bb02f2bc07270d95dbb28e1fe322a3ace1ba6
Maxim Söhngen <Maxim.Soehngen@uni-duesseldorf.de>
2013-04-30 23:50:51 +0200

    Spielfeld in main importiert, levels in main erstellen lassen

--- gruppe32/src/main.java
+++ gruppe32/src/main.java
@@ -1,4 +1,4 @@
-
+import Spielfeld.*;
 public class main {
 
 	/**
@@ -6,6 +6,7 @@ public class main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Spielfeld.levelsErstellen();
 
 	}
 

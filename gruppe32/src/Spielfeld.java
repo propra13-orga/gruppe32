4f5accfb7de4a5d83daec0024224969347e69359
Maxim Söhngen <Maxim.Soehngen@uni-duesseldorf.de>
2013-05-03 17:10:38 +0200

    Spielfeld.levelsErstellen verbessert

--- gruppe32/src/Spielfeld.java
+++ gruppe32/src/Spielfeld.java
@@ -8,24 +8,23 @@ int spalte;
 /*muss in der main-class direkt am anfang ausgefuehrt werden*/
 public void levelsErstellen() {
 /*Allen Randfeldern in allen Leveln den Wert 1 ("hier is ne mauer") geben*/
-	for (spalte=0; (spalte==0)||(spalte==19); spalte++)
-	{
-		for (reihe=0; reihe<=14; reihe++) {
-			spielfeld[0][spalte][reihe]=1;
-			spielfeld[1][spalte][reihe]=1;
-			spielfeld[2][spalte][reihe]=1;
-			spielfeld[3][spalte][reihe]=1;
+	for (spalte=0; spalte<20; spalte++) {
+		for (reihe=0; reihe<15; reihe++) {
+			for (level=0; level<4; level++) {
+				if ((spalte==0)|(spalte==19)|(reihe==0)|(reihe==14)) {			
+					spielfeld[level][spalte][reihe]=1;
+					
+				}
+				else {
+					spielfeld[level][spalte][reihe]=0;
+				}
+			}
 		}
 	}
-	for (reihe=0; (reihe==0)||(reihe==14); spalte++)
-	{
-		for (spalte=0; spalte<=19; spalte++) {
-			spielfeld[0][spalte][reihe]=1;
-			spielfeld[1][spalte][reihe]=1;
-			spielfeld[2][spalte][reihe]=1;
-			spielfeld[3][spalte][reihe]=1;
-		}
-	}
+	
+	
+		
+	
 	//startfelder(2) und zielfelder(3) setzen, erstmal fuer alle level das gleiche
 	spielfeld[0][0][5]=spielfeld[1][0][5]=spielfeld[2][0][5]=spielfeld[3][0][5]=2;
 	spielfeld[0][19][5]=spielfeld[1][19][5]=spielfeld[2][19][5]=spielfeld[3][19][5]=3;

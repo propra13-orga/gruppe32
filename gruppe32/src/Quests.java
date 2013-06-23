115a9b8ab86e47de36c7b6e6133e9ff05c283c5f
Christine Nowak <Christine.Nowak@hhu.de>
2013-06-23 13:49:19 +0200

    Klasse Quests erstellt

---
+++ gruppe32/src/Quests.java
@@ -0,0 +1,17 @@
+
+public class Quests {
+
+	/**
+	 * Vornotizen:
+	 * 
+	 * 1. Quest: Sammeln von Gegenständen, um weiter zu kommen, Leitfigur ist Storyteller
+	 * 
+	 * 2. Quest: Rätsel: Mauerstein als versteckter Durchgang, evtl. Storytellertext als Hinweis 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+	
+	
+}
